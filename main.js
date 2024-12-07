9bd5ba57a74c6d90274d7bdab8ab4a06ec25aeae
sinan684 <asifsinan87@gmail.com>
2024-12-06 18:29:32 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var y=parseInt(prompt(' type a number '))
+
+let x=parseInt(prompt(' type another number '))
+
+console.log(y+x)
+
+/*const naam=prompt( 'whats your name ')
+
+console.log(naam)
+
+let a=prompt('Your Total')
+
+console.log(parseFloat(a) * 30 ) /100 ; 
+
+finalAmount=parseFloat(a) - (a) *10 / 100*/
+
+const pet=prompt('Name your favorite animal')
+
+let comment=(' is a good animal')
+
+console.log(pet+comment)
